5da15a9b564a2d91026eb8ffff7d0e69cda00acd
ellenmariadev <ellenmariadev@gmail.com>
2023-08-30 03:53:45 -0300

    test: jest setup + dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["mediacloud.carbuyer.co.uk"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
