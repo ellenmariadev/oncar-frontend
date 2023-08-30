54add785fa6211dff2ef3d65a0c25733f314f8c8
ellenmariadev <ellenmariadev@gmail.com>
2023-08-30 04:05:40 -0300

    test: mockData

---
+++ __tests__/mock/mockData.ts
@@ -0,0 +1,18 @@
+export const mockVehicleData = {
+  data: [
+    {
+      id: 1,
+      brand: "Mercedes",
+      model: "AMG GLE 63 Coup",
+      price: "145450",
+      year: 2023,
+    },
+    {
+      id: 2,
+      brand: "Honda",
+      model: "Civic",
+      price: "200000",
+      year: 2019,
+    },
+  ],
+};
